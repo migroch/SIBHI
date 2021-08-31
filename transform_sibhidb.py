e3fd86fb4788377fa1e935cb5d27df2885f864fd
Miguel Rocha <migroch@gmail.com>
2021-08-31 14:30:47 -0700

    Added District labels and merge reverse encoding table with the resources table

--- transform_sibhidb.py
+++ transform_sibhidb.py
@@ -15,6 +15,7 @@ var_info = pd.read_csv('data/SIBHI Survey Codebook - Variable info.csv')
 #col_rename_dict = var_info[['Excel variable label','description']].set_index('Excel variable label').to_dict()['description']
 col_rename_dict = {'Import?':'Import', 'Resp#':'RespNum', 'SchID.1':'SchID_1', 'DisID.1':'DisID_1' }
 resources = resources.rename(columns=col_rename_dict)
+resources['DisID'] = resources.DisID.astype(pd.Int64Dtype())
 inputs = inputs.rename(columns=col_rename_dict)
 
 # Replace numerical values with text values in codebook
@@ -24,6 +25,7 @@ res_values = res_values.rename(   # codebook dictionary
 num_val_cols = res_values['var_label'].unique()
 
 def mapper(col, num_val):
+    if  type(num_val) != str and pd.isnull(num_val) == False:  num_val = str(int(num_val))
     val = res_values.query('var_label==@col and num_val==@num_val').res_label 
     if len(val): return val.iloc[0] 
     else: return num_val 
@@ -36,6 +38,9 @@ for col in inputs.columns:
     if col in num_val_cols:
         inputs[col] = inputs[col].apply(lambda x: mapper(col, x))
 
+# Set DisID = SchID if DisID is null        
+resources['DisID'][resources.DisID.isna()] = resources['SchID'][resources.DisID.isna()]
+ 
 # Convert dtypes
 for col in resources.columns: 
     if resources[col].dtype.name=='float64': resources[col]=resources[col].astype('Int64').astype('str') 
@@ -48,8 +53,8 @@ resources = resources.replace({'99':None, 99:pd.np.nan, '98':'Invalid Input', 98
 inputs = inputs.replace({'99':None, 99:pd.np.nan, '98':'Invalid Input', 98:pd.np.nan, '97':'Invalid Input', 97:pd.np.nan, '96':'Invalid Input', 96:pd.np.nan})
 
 # Reverse encoding
+resources['EntryID'] = resources.index
 resources_copy = resources.copy()
-resources_copy['EntryID'] = resources_copy.index
 en_dict = {'GradeLevels':['GrdK5','Grd68','Grd912','GrdNA','GrdDK','GrdOTH'],
            'IntendedPurpose':['PurposeMH','PurposeSEL','PurposePSC','PurposeSUP','PurposeTUPE','PurposeNA','PurposeDK','PurposeOTH'],
            'ResourceFormat':['ResfmtINDIV','ResfmtFAM','ResfmtGRP','ResfmtCR','ResfmtCAMPUS','ResfmtNA','ResfmtDK','ResfmtOTH'],
@@ -74,9 +79,10 @@ for key in en_dict:
         rev_resources = new_df
     else:
         rev_resources = pd.merge(rev_resources, new_df, on=['EntryID','ResourceType'], how='outer')
-
     count += 1
 
+rev_resources = rev_resources.merge(resources, on=['EntryID'], how='left')     
+    
 # Write csv files
 resources.to_csv('data/resources.csv', index=False)
 rev_resources.to_csv('data/rev_resources.csv', index=False)
@@ -86,7 +92,7 @@ inputs.to_csv('data/respondent_inputs.csv', index=False)
 pandas_gbq.to_gbq(resources, "sibhiDB.resources", if_exists='replace', project_id="sibhi-survey-resources",
                   table_schema=schemas.resources_schema
                   )
-pandas_gbq.to_gbq(rev_resources, "sibhiDB.resources", if_exists='replace', project_id="sibhi-survey-resources",
+pandas_gbq.to_gbq(rev_resources, "sibhiDB.rev_resources", if_exists='replace', project_id="sibhi-survey-resources",
                   table_schema=schemas.rev_schema)
 pandas_gbq.to_gbq(inputs, "sibhiDB.respondent_inputs", if_exists='replace', project_id="sibhi-survey-resources",
                   table_schema=schemas.inputs_schema
